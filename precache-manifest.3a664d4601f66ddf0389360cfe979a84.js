23a610ed85ce7e600a6efe9d36fad76a230e58ac
Srikanth Reddy Gubballi <gubballi.s@northeastern.edu>
2020-05-21 23:55:04 -0400

    Updates

---
+++ precache-manifest.3a664d4601f66ddf0389360cfe979a84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07dc4be7a18d48b02a8bd10d71c98ae9",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "4e0e518464ce727a513c",
+    "url": "/My-Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9838e08340eca0280835",
+    "url": "/My-Portfolio/static/css/main.bbea7da6.chunk.css"
+  },
+  {
+    "revision": "4e0e518464ce727a513c",
+    "url": "/My-Portfolio/static/js/2.10561fde.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/My-Portfolio/static/js/2.10561fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9838e08340eca0280835",
+    "url": "/My-Portfolio/static/js/main.df8cf571.chunk.js"
+  },
+  {
+    "revision": "b94d711fd9029e1124cd",
+    "url": "/My-Portfolio/static/js/runtime-main.afe79d49.js"
+  },
+  {
+    "revision": "dfddae72398d056a3730c12cdaf4fb65",
+    "url": "/My-Portfolio/static/media/me.dfddae72.jpg"
+  }
+]);
